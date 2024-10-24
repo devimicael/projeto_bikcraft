9f0e928777204c9692f3a3c4c0e3af3817ac449d
devimicael <d7096439@gmail.com>
2024-10-24 11:50:51 -0300

    feat: perguntas frequentes

--- src/js/script.js
+++ src/js/script.js
@@ -21,3 +21,24 @@ function ativarProduto(parametro) {
 }
 
 parametros.forEach(ativarProduto);
+
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+
+function ativarPergunta(_event) {
+  const pergunta = _event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+
+  
+  const resposta = document.getElementById(controls);
+  resposta.classList.toggle("ativo");
+  const ativo = resposta.classList.contains("ativo");
+  pergunta.setAttribute("aria-expanded", ativo);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
