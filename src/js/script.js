9b9bb60f5b012ed9644f54cfe569637ba70d22eb
devimicael <d7096439@gmail.com>
2024-10-24 11:02:59 -0300

    feat: enviando informações entre páginas

--- src/js/script.js
+++ src/js/script.js
@@ -10,3 +10,14 @@ function ativarlinks(link) {
 }
 
 links.forEach(ativarlinks);
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if(elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
